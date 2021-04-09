5f6ee535e0c68ae9f3a994586f24b49df48d93dc
KyrieErwin <59181361+tommylin121314@users.noreply.github.com>
2021-04-08 20:57:45 -0400

    Selected list is on top

--- client/src/components/sidebar/SidebarList.js
+++ client/src/components/sidebar/SidebarList.js
@@ -2,11 +2,32 @@ import React        from 'react';
 import SidebarEntry from './SidebarEntry';
 
 const SidebarList = (props) => {
+    let lists = [];
+    props.todolists.map((list,index) => lists[index] = list);
+
+    if(props.activeid){
+        let index = -1;
+        for(let i = 0; i < lists.length; i++) {
+            if(lists[i].id === props.activeid) {
+                index = i;
+                break;
+            }
+        }
+        for(let i = index; i > 0; i--) {
+            let temp = lists[i];
+            lists[i] = lists[i-1];
+            lists[i-1] = temp;
+        }
+    }
+
+    console.log("HAS ACTIVE LIST: " + props.activeid);
+    console.log(lists);
+
     return (
         <>
             {
                 props.todolists &&
-                props.todolists.map(todo => (
+                lists.map(todo => (
                     <SidebarEntry
                         handleSetActive={props.handleSetActive} activeid={props.activeid}
                         id={todo.id} key={todo.id} name={todo.name} _id={todo._id}
